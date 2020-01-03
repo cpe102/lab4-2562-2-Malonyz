6bdd3120cb9a9ff5ff810052d39d3c3e31d185f5
Malonyz <mapmap1234@hotmail.com>
2020-01-03 15:51:44 +0700

    lab4_2

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
+float find(float x){
+    float y,e=2.71828;
+    y=(3*pow(x,3))+(2*e)+(pow(2,(2*x+1)))+(sqrt(pow(x,2)+1));
+    cout<<y;
+}
+
 int main(){
+    float x;
+    cout << "Enter x =";
+    cin >> x;
+    find(x);
+
+
     
+
 }
